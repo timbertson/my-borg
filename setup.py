f8bc19ac10e6d14c9101ffcca3c522e1895fa69b
Tim Cuthbertson <tim@gfxmonk.net>
2017-01-17 20:18:42 +1100

    initial

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+def read(relpath):
+	with open(path.join(here, *relpath.split('/')), encoding='utf-8') as f:
+		return f.read()
+
+setup(
+	name='my-borg',
+	version=read('VERSION'),
+	description='TODO',
+	packages=find_packages(exclude=['doc', 'test']),
+	install_requires=['borg'],
+	scripts=['bin/my-borg'],
+)
+
